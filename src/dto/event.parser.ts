f26fada3a1addbedfa4babaa416345e5380a2464
joney <joneywon666@gmail.com>
2025-02-15 21:06:17 +0800

    update

--- src/dto/event.parser.ts
+++ src/dto/event.parser.ts
@@ -102,8 +102,10 @@ export class EventParser {
                 pfc.mint = data.mint.toString()
                 pfc.bondingCurve = data.bondingCurve.toString()
                 pfc.userAdr = data.user.toString()
-                pfc.name = '' //data.name?.replace(/\u0000/g, '')??'';
-                pfc.symbol = '' //data.symbol?.replace(/\u0000/g, '')??'';
+                pfc.name = data.name?.replace(/\u0000/g, '')??'';
+                pfc.symbol = data.symbol?.replace(/\u0000/g, '')??'';
+                //pfc.name = data.name
+                //pfc.symbol = data.symbol
                 pfc.uri = data.uri
                 pfc.txId = txId
                 pfc.blockNumber = blockNumber
